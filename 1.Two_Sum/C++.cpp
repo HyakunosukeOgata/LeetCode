1d92818d8534efe4e2e92e22a1ea168be1b04b43
Ogata <renpung30827@gmail.com>
2023-01-07 14:09:02 +0800

    Hash table and Double loop

--- 1.Two_Sum/C++.cpp
+++ 1.Two_Sum/C++.cpp
@@ -1,16 +1,36 @@
-#include <iostream>
-using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+Runtime 17 ms   Beats   78.26%
+Memory  10.8 MB Beats   48.60%
 
-int main()  
-{  
-    int ptr1[5] = {3,1,2,6,9}; // integer array declaration  
-    int *p;
-    p = &ptr1[0];
-	cout << &ptr1[0] << endl;
-	cout << &ptr1[1] << endl; 
-	cout << &ptr1[2] << endl;  
-	for (int i = 0; i < 5; i++){
-		cout << *p++ << endl;
-	}
-}  
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> map;
+        for (int i = 0; i < nums.size(); i++){
+            if (map.find(target - nums[i]) != map.end()){
+                return {i,map[target- nums[i]]};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
+Runtime 688 ms  Beats   23.73%
+Memory  10.2 MB Beats   74.3%
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> ans;
+        for (int i = 0 ; i < nums.size(); i++){
+            for (int j = i+1; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
