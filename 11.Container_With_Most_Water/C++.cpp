01210f168183173f277095a41fe00ba2def90de2
Ogata <renpung30827@gmail.com>
2023-03-29 00:49:59 +0800

    2023/03/29

---
+++ 11.Container_With_Most_Water/C++.cpp
@@ -0,0 +1,19 @@
+Runtime 103 ms Beats 43.50% 
+Memory  59  MB Beats 57.6%
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left  = 0;
+        int right = height.size() - 1;
+        int ans = 0;
+        while(left < right){
+            ans = max(min(height[left], height[right])* (right - left), ans);
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        } 
+        return ans;
+    }
+};
