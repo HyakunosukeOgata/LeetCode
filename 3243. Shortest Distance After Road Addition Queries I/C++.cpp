3ab06a99bdde5408ce2320f18244ac019bbaf6cb
Ogata <renping30827@gmail.com>
2024-11-28 01:50:29 +0800

    2024/11

--- 3243. Shortest Distance After Road Addition Queries I/C++.cpp
+++ 3243. Shortest Distance After Road Addition Queries I/C++.cpp
@@ -2,35 +2,30 @@ class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
         vector<int> dp(n, 0);
-
         vector<int> adj[505];
-
         vector<int> ans;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++){
             dp[i] = i;
-            if (i != 0) {
-                adj[i].push_back(i - 1); 
-            }
+            if (i != 0) 
+                adj[i].push_back(i - 1);            
         }
 
-        for (auto& query : queries) {
-            int start = query[0];
-            int end = query[1];
-
+        for (int i = 0; i < queries.size(); i++){
+            int start = queries[i][0];
+            int end   = queries[i][1];
             adj[end].push_back(start);
 
-            for (int i = 0; i < n; i++) {
-                for (int& neighbor : adj[i]) {
-                    if (dp[i] > dp[neighbor] + 1) {
-                        dp[i] = dp[neighbor] + 1;
+            for (int j = 0; j < n; j++){
+                for (int k = 0; k < adj[j].size(); k++) {
+                    if (dp[j] > dp[adj[j][k]] + 1){
+                        dp[j] = dp[adj[j][k]] + 1;
                     }
                 }
             }
-
             ans.push_back(dp[n - 1]);
         }
 
-        return ans; 
+        return ans;
     }
 };
