57d0de5484c3fa9f1fc8b6955a8d484244041ef9
Ogata <renping30827@gmail.com>
2025-03-29 00:25:47 +0800

    2025/03

--- 2.Add_Two_Numbers/C++.cpp
+++ 2.Add_Two_Numbers/C++.cpp
@@ -1,31 +1,41 @@
-Runtime 39 ms   Beats 81.7%
-Memory  71.6 MB Beats 13.41%
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *dummy = new ListNode();
-        ListNode *temp = dummy;
-        int carry = 0;
-        while (l1 || l2 || carry){
-            int sum = 0;
-            if (l1 != NULL){
-                sum += l1->val;
-                l1 = l1->next;
+    public:
+        ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+            int carry = 0;
+            struct ListNode* dummy = new ListNode();
+            struct ListNode* curr = dummy;
+            while(l1 || l2 || carry){
+                int sum = 0;
+                if (l1){
+                    sum += l1->val;
+                    l1 = l1 -> next;
+                }
+                if (l2){
+                    sum += l2->val;
+                    l2 = l2 -> next;
+                }
+                if (carry)
+                    sum += carry;
+    
+                if (sum > 9){
+                    carry = 1;
+                    sum -= 10;
+                }else{
+                    carry = 0;
+                }
+                curr -> next = new ListNode(sum);
+                curr = curr -> next;
             }
-            if (l2 != NULL){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum += carry;
-            carry = sum / 10;
-            ListNode* newNode = new ListNode(sum%10);
-            temp -> next = newNode;
-            temp = temp -> next;
+            return dummy->next;
         }
-        return dummy->next;
-    }
-};
-
-
-
+    };
