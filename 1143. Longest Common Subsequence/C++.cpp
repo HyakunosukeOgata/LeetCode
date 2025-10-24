579b23e73ba9e26b48fb5fd2b8e26ac850dfcd3c
Ogata <renping30827@gmail.com>
2025-10-24 23:14:06 +0800

    Leetcode, 1143, dp

--- 1143. Longest Common Subsequence/C++.cpp
+++ 1143. Longest Common Subsequence/C++.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp(text1.size() + 1, vector<int> (text2.size() + 1, 0));
- 
-        for (int i = 1; i <= text1.size(); ++i){
-            for (int j = 1; j <= text2.size(); ++j){
-                if (text1[i-1]==text2[j-1]){
+        int n1 = text1.size();
+        int n2 = text2.size();
+        int result = 0;
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+        for (int i = 1; i <= n1; ++i){
+            for (int j = 1; j <= n2; ++j){
+                if (text1[i-1] == text2[j-1]){
                     dp[i][j] = dp[i-1][j-1] + 1;
-                }
-                else{
+                    result = result > dp[i][j] ? result : dp[i][j];
+                }else{
                     dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
                 }
             }
         }
-        return dp[text1.size()][text2.size()];
+        return result;
     }
 };
