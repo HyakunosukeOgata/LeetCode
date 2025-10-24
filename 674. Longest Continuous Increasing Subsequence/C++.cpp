617c82d99e942fb641249f05611e42cf5e9cf54f
Ogata <renping30827@gmail.com>
2025-10-24 22:21:00 +0800

    Leetcode, 674, dp and linear scan

--- 674. Longest Continuous Increasing Subsequence/C++.cpp
+++ 674. Longest Continuous Increasing Subsequence/C++.cpp
@@ -13,3 +13,19 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int findLengthOfLCIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        int ans = 1;
+        for (int i = 1; i < n; i++){
+            if (nums[i] > nums[i-1]){
+                dp[i] = dp[i - 1] + 1;
+                ans = ans > dp[i] ? ans : dp[i];
+            }
+        }
+        return ans;
+    }
+};
