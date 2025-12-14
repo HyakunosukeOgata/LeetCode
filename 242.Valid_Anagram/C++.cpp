20cc96b547b41d06adb79417863f26eb211a0445
Ogata <renping30827@gmail.com>
2025-12-15 00:08:18 +0800

    update, different method

--- 242.Valid_Anagram/C++.cpp
+++ 242.Valid_Anagram/C++.cpp
@@ -1,39 +1,58 @@
 class Solution {
 public:
+	// Time: O(nlogn)
+    // Space: O(1)
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()){
+        if (s.size() != t.size()){
             return false;
         }
-        unordered_map <char, int> letter;
-        for (int index = 0; index < s.length(); index++){
-            letter[s[index]] ++;
-            letter[t[index]] --;
-        }
-        for (auto s:letter){
-            if (s.second != 0)
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] != t[i]){
                 return false;
+            }
         }
         return true;
-        
     }
-};
-
-//-----------------------------------------------------------------------
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+	
+	// Time: O(n)
+    // Space: O(1)
+	bool isAnagram(string s, string t) {
+        if (s.size() != t.size()){
             return false;
         }
+		unordered_map<char, int> map;   
+		int n = s.size();
+		for (int i = 0; i < n; i++){
+			map[s[i]] ++;
+		}
+		for (int i = 0; i < n; i++){
+			if (map.find(t[i]) == map.end()){
+				return false;
+			}
+			else if (map[t[i]] > 0){
+				map[t[i]] --;
+			}else {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// Time: O(n)
+    // Space: O(1)
+	bool isAnagram(string s, string t) {
+		if (s.size() != t.size()) 
+			return false;
 
-        vector<int> table_s(26, 0); 
-        vector<int> table_t(26, 0); 
-
-        for (int index = 0; index < s.length(); index++) {
-            table_s[s[index] - 'a']++;
-            table_t[t[index] - 'a']++;
-        }
-
-        return table_s == table_t;
-    }
+		int cnt[26] = {0};
+		for (char c : s) 
+			cnt[c - 'a']++;
+		for (char c : t) {
+			if (--cnt[c - 'a'] < 0) 
+				return false;
+		}
+		return true;
+	}
 };
