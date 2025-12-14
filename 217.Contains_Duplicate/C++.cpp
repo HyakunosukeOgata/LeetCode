e5e91de10125e5c0f7b2a67504736f19dfe679ae
Ogata <renping30827@gmail.com>
2025-12-14 14:49:11 +0800

    update, diff method

--- 217.Contains_Duplicate/C++.cpp
+++ 217.Contains_Duplicate/C++.cpp
@@ -1,19 +1,45 @@
-Runtime 161 ms Beats 36.71% 
-Memory 71.6 MB Beats 16.69%
-
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map <int, int> mp;
-        int vector_nums_length = nums.size();
-        for (int index = 0; index < vector_nums_length; index++){
-            mp[nums[index]] += 1;
-        }
-        for (int index = 0; index < vector_nums_length; index++){
-            if (mp[nums[index]] > 1){
+    // Time: O(n log n)
+    // Space: O(1)（忽略 sort stack）
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());  
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] == nums[i - 1]){
                 return true;
             }
         }
         return false;
     }
+	
+	// Time: O(nlogn), 紅黑樹高度被保證為 O(log n)，不存在退化情況。
+    // Space: O(n)
+	bool hasDuplicate(vector<int>& nums) {
+		set<int> nums_set(nums.begin(), nums.end());
+		return nums_set.size() != nums.size();
+	}
+
+	//	Time: Best: O(1), Average: O(n), Worst: O(n²)
+	//	Space: O(n)
+	bool hasDuplicate(vector<int>& nums) {
+		unordered_set<int> s;
+		for (int x : nums) {
+			if (s.count(x)) 
+				return true;
+			s.insert(x);
+		}
+		return false;
+	}
+	
+	// 	Time: Best: O(1), Average: O(n), Worst: O(n)
+	// 	Space: O(range)
+	bool hasDuplicate(vector<int>& nums) {
+		vector<bool> seen(100001, false);
+		for (int x : nums) {
+			if (seen[x]) 
+				return true;
+			seen[x] = true;
+		}
+		return false;
+	}
 };
